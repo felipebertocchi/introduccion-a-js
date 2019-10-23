abee1c830405a16f4d3ec057877c94755231a230
Felipe Bertocchi <fabertocchi@gmail.com>
2019-10-23 14:13:08 -0300

    Update tarea-listas
    Mejora y correción de errores según comentarios en pull request #2

--- tareas/clase-5/tarea-listas.js
+++ tareas/clase-5/tarea-listas.js
@@ -1,5 +1,5 @@
-const nodoLista = document.querySelector('ol');
-const nodoResultados = document.querySelector('em');
+const $nodoLista = document.querySelector('ol');
+const $nodoResultados = document.querySelector('em');
 
 const $generarLista = document.querySelector("#generar-lista");
 
@@ -11,16 +11,24 @@ const $calcularMasFrecuente = document.querySelector("#calcular-mas-frecuente");
 let lista;
 
 $generarLista.onclick = function () {
-    n = Number(document.querySelector("#cantidad-numeros").value)
-    for (let i = 0; i < n; i++) {
-        let numeroAleatorio = Math.round(Math.random() * 100)
-        let nuevoElemento = document.createElement('li');
-        let numero = document.createTextNode(numeroAleatorio);
-        nuevoElemento.appendChild(numero);
-        nodoLista.appendChild(nuevoElemento);
+    const n = Number(document.querySelector("#cantidad-numeros").value)
+    if (n === 0) {
+        alert("No se ingresó ningún valor");
     }
-    lista = document.querySelectorAll("li");
-    return lista;
+    else if (n > 0) {
+        for (let i = 0; i < n; i++) {
+            let numeroAleatorio = Math.round(Math.random() * 100)
+            let $nuevoElemento = document.createElement('li');
+            let numero = document.createTextNode(numeroAleatorio);
+            $nuevoElemento.appendChild(numero);
+            $nodoLista.appendChild($nuevoElemento);
+        }
+        lista = document.querySelectorAll("li");
+        return lista;
+    }
+    else {
+        alert("El número ingresado no es válido");
+    }
 }
 
 function calcularPromedio(lista) {
@@ -53,59 +61,80 @@ function calcularMasGrande(lista) {
 }
 
 function calcularMasFrecuente(lista) {
-    let masFrecuente = Number(lista[0].textContent);
-    let mf = 1;
-    var m = 0;
+    let masFrecuente = "ninguno";
+    let vecesRepetido = 0;
+    let masVecesRepetido = 0;
     for (let i = 0; i < lista.length; i++) {
-        for (let j = i; j < lista.length; j++) {
-            if (Number(lista[i].textContent) == Number(lista[j].textContent))
-                m++;
-            if (mf < m) {
-                mf = m;
+        for (let j = i + 1; j < lista.length; j++) {
+            if (Number(lista[i].textContent) === Number(lista[j].textContent)) {
+                vecesRepetido++;
+            }
+            if (vecesRepetido > masVecesRepetido) {
+                masVecesRepetido = vecesRepetido;
                 masFrecuente = Number(lista[i].textContent);
             }
         }
-        m = 0;
+        vecesRepetido = 0;
     }
-        return (`${masFrecuente} (se repite ${mf} veces)`);
+    return (`${masFrecuente} (se repite ${masVecesRepetido} veces)`);
 }
 
 $calcularPromedio.onclick = function () {
-    let promedio = calcularPromedio(lista);
-    let nuevaDivision = document.createElement('div');
-    let nuevoElemento = document.createElement('strong');
-    let nodoTexto = document.createTextNode(`El promedio es de: ${promedio}`);
-    nuevoElemento.appendChild(nodoTexto);
-    nuevaDivision.append(nuevoElemento)
-    nodoResultados.appendChild(nuevaDivision);
+    if (lista === undefined) {
+        alert("Error: Todavía no se generó una lista de números");
+    }
+    else {
+        let promedio = calcularPromedio(lista);
+        let $nuevaDivision = document.createElement('div');
+        let $nuevoElemento = document.createElement('strong');
+        let nodoTexto = document.createTextNode(`El promedio es de: ${promedio}`);
+        $nuevoElemento.appendChild(nodoTexto);
+        $nuevaDivision.append($nuevoElemento)
+        $nodoResultados.appendChild($nuevaDivision);
+    }
 }
 
 $calcularMasGrande.onclick = function () {
-    let masGrande = calcularMasGrande(lista);
-    let nuevaDivision = document.createElement('div');
-    let nuevoElemento = document.createElement('strong');
-    let nodoTexto = document.createTextNode(`El número más grande es: ${masGrande}`);
-    nuevoElemento.appendChild(nodoTexto);
-    nuevaDivision.append(nuevoElemento)
-    nodoResultados.appendChild(nuevaDivision);
+    if (lista === undefined) {
+        alert("Error: Todavía no se generó una lista de números");
+    }
+    else {
+        let masGrande = calcularMasGrande(lista);
+        let $nuevaDivision = document.createElement('div');
+        let $nuevoElemento = document.createElement('strong');
+        let nodoTexto = document.createTextNode(`El número más grande es: ${masGrande}`);
+        $nuevoElemento.appendChild(nodoTexto);
+        $nuevaDivision.append($nuevoElemento)
+        $nodoResultados.appendChild($nuevaDivision);
+    }
 }
 
 $calcularMasPequenio.onclick = function () {
-    let masPequenio = calcularMasPequenio(lista);
-    let nuevaDivision = document.createElement('div');
-    let nuevoElemento = document.createElement('strong');
-    let nodoTexto = document.createTextNode(`El número más pequeño es: ${masPequenio}`);
-    nuevoElemento.appendChild(nodoTexto);
-    nuevaDivision.append(nuevoElemento)
-    nodoResultados.appendChild(nuevaDivision);
+    if (lista === undefined) {
+        alert("Error: Todavía no se generó una lista de números");
+    }
+    else {
+        let masPequenio = calcularMasPequenio(lista);
+        let $nuevaDivision = document.createElement('div');
+        let $nuevoElemento = document.createElement('strong');
+        let nodoTexto = document.createTextNode(`El número más pequeño es: ${masPequenio}`);
+        $nuevoElemento.appendChild(nodoTexto);
+        $nuevaDivision.append($nuevoElemento)
+        $nodoResultados.appendChild($nuevaDivision);
+    }
 }
 
 $calcularMasFrecuente.onclick = function () {
-    let masFrecuente = calcularMasFrecuente(lista);
-    let nuevaDivision = document.createElement('div');
-    let nuevoElemento = document.createElement('strong');
-    let nodoTexto = document.createTextNode(`El número más frecuente es: ${masFrecuente}`);
-    nuevoElemento.appendChild(nodoTexto);
-    nuevaDivision.append(nuevoElemento)
-    nodoResultados.appendChild(nuevaDivision);
+    if (lista === undefined) {
+        alert("Error: Todavía no se generó una lista de números");
+    }
+    else {
+        let masFrecuente = calcularMasFrecuente(lista);
+        let $nuevaDivision = document.createElement('div');
+        let $nuevoElemento = document.createElement('strong');
+        let nodoTexto = document.createTextNode(`El número más frecuente es: ${masFrecuente}`);
+        $nuevoElemento.appendChild(nodoTexto);
+        $nuevaDivision.append($nuevoElemento)
+        $nodoResultados.appendChild($nuevaDivision);
+    }
 }
