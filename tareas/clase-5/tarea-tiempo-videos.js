c9f0fcfec503fd289f5d4c3b9ddfcb22bab191b3
Felipe Bertocchi <fabertocchi@gmail.com>
2019-10-26 10:35:37 -0300

    Update tarea-tiempo-videos.js

--- tareas/clase-5/tarea-tiempo-videos.js
+++ tareas/clase-5/tarea-tiempo-videos.js
@@ -8,7 +8,7 @@ let segundos = 0;
 let horasTotal = 0;
 let minutosTotal = 0;
 let segundosTotal = 0;
-const nodoPagina = document.querySelector('div');
+const $nodoPagina = document.querySelector('div');
 
 
 $ingresar.onclick = function () {
@@ -29,18 +29,18 @@ $ingresar.onclick = function () {
         minutosTotal -= 60;
     }
 
-    const nuevoParrafo = document.createElement('p');
+    const $nuevoParrafo = document.createElement('p');
     const textoParrafo = document.createTextNode(`Ingresaste un tiempo de ${horas}:${minutos}:${segundos}`);
-    nuevoParrafo.appendChild(textoParrafo);
-    nodoPagina.appendChild(nuevoParrafo);
+    $nuevoParrafo.appendChild(textoParrafo);
+    $nodoPagina.appendChild($nuevoParrafo);
 
-    return horas, minutos, segundos;
+    return [horas, minutos, segundos];
 }
 
 $calcularTiempo.onclick = function () {
     horasTotal = (horasTotal < 10) ? "0" + horasTotal : horasTotal;
     minutosTotal = (minutosTotal < 10) ? "0" + minutosTotal : minutosTotal;
     segundosTotal = (segundosTotal < 10) ? "0" + segundosTotal : segundosTotal;
-    let tiempoTotal = horasTotal + ":" + minutosTotal + ":" + segundosTotal;
+    let tiempoTotal = `${horasTotal}:${minutosTotal}:${segundosTotal}`;
     document.querySelector("#tiempo-total").append(tiempoTotal);
 }
